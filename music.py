ddb57abfedb2656cb89b05274e209965f401f90b
yoonguk510 <yoonguk510@gmail.com>
2018-02-05 23:14:47 +0900

    music.py

---
+++ music.py
@@ -0,0 +1,33 @@
+import os
+import random
+import subprocess
+
+def get_files(root):
+    files = []
+
+    def scan_dir(dir):
+        for f in os.listdir(dir):
+            f = os.path.join(dir, f)
+            if os.path.isdir(f):
+                scan_dir(f)
+            elif os.path.splitext(f)[1] == ".mp3":
+                files.append(f)
+
+    scan_dir(root)
+    return files
+
+# double-buffered playlists so there aren't any repeats
+buffers = (get_files("/home/yoonguk510/Desktop/Jarvis/music"), [])
+playing = 0
+
+while True:
+    if len(buffers[playing]) > 0:
+        f = random.choice(buffers[playing])
+        buffers[playing].remove(f)
+        buffers[not playing].append(f)
+    else:
+        playing = not playing
+        continue
+    p = subprocess.Popen(["mpg123", f])
+    p.wait()
+
